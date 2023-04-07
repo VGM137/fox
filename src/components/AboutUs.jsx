500a31db24afb5ae286d69ba0e39609c390d3fe4
Victor Bueno <victorgbueno.castro@hotmail.com>
2023-04-06 22:20:36 -0600

    new changes in navbar and hero

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import '../assets/styles/Components/Footer.css'
+
+const AboutUs = () => {
+
+  return (
+    <div id="about">
+        <h1 style={{height:"1000px"}}>about us</h1>
+        {/* <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur nesciunt optio necessitatibus illum ab eius sequi libero, nemo ipsa inventore at exercitationem aliquid aliquam soluta! Facere natus tempore laboriosam fugiat!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur nesciunt optio necessitatibus illum ab eius sequi libero, nemo ipsa inventore at exercitationem aliquid aliquam soluta! Facere natus tempore laboriosam fugiat!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur nesciunt optio necessitatibus illum ab eius sequi libero, nemo ipsa inventore at exercitationem aliquid aliquam soluta! Facere natus tempore laboriosam fugiat!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur nesciunt optio necessitatibus illum ab eius sequi libero, nemo ipsa inventore at exercitationem aliquid aliquam soluta! Facere natus tempore laboriosam fugiat!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur nesciunt optio necessitatibus illum ab eius sequi libero, nemo ipsa inventore at exercitationem aliquid aliquam soluta! Facere natus tempore laboriosam fugiat!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur nesciunt optio necessitatibus illum ab eius sequi libero, nemo ipsa inventore at exercitationem aliquid aliquam soluta! Facere natus tempore laboriosam fugiat!</p> */}
+
+    </div>
+    // <div className='row w-100 m-auto mt-50 p-0 footer-container'>
+    // </div>
+  )
+}
+
+export default AboutUs
